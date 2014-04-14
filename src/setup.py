553e4efe03e779f069cb4a28ff7bb1073cd49b20
Scott Johnson <jaywir3@gmail.com>
2014-04-14 18:44:11 -0500

    Issue #18: Made setup.py install subpackages as necessary for transgression to work.

--- src/setup.py
+++ src/setup.py
@@ -1,25 +1,39 @@
 from setuptools import setup, find_packages
 import os
 
-progName = 'transgression'
-progVersion = '0.0.4'
-progDescription='Generic binary regression finding utility based off of mozregression'
-progAuthor = 'Scott Johnson'
-progEmail = 'jaywir3@gmail.com'
-progUrl = 'http://github.com/jwir3/transgression'
-entry_points = { 'console_scripts': [
-  'transgression = transgression.transgression:main',
-]}
+setup(name='prettylogger',
+      version='0.0.1',
+      description='A logging utility package designed for command line use',
+      author='Scott Johnson',
+      author_email='jaywir3@gmail.com',
+      url='http://github.com/jwir3/transgression',
+      packages=['prettylogger'],
+      install_requires=['argparse', 'ansicolors', 'httplib2', 'mozfile',
+                        'mozprofile', 'mozrunner', 'BeautifulSoup', 'paramiko']
+)
 
-setup(name=progName,
-      version=progVersion,
-      description=progDescription,
-      author=progAuthor,
-      author_email=progEmail,
-      url=progUrl,
-      packages=find_packages(),
-      entry_points=entry_points,
+setup(name='configurator',
+      version='0.0.1',
+      description='Configuration utility designed to simplify XML configuration',
+      author='Scott Johnson',
+      author_email='jaywir3@gmail.com',
+      url='http://github.com/jwir3/transgression',
+      packages=['configurator'],
       install_requires=['argparse', 'ansicolors', 'httplib2', 'mozfile',
                         'mozprofile', 'mozrunner', 'BeautifulSoup', 'paramiko',
-                        'unittest']
+                        'prettylogger']
+)
+
+setup(name='transgression',
+      version='0.0.1',
+      description='Generic binary regression finding utility based off of mozregression',
+      author='Scott Johnson',
+      author_email='jaywir3@gmail.com',
+      url='http://github.com/jwir3/transgression',
+      packages=['transgression'],
+      entry_points={ 'console_scripts': [
+        'transgression = transgression.transgression:main'] },
+      install_requires=['argparse', 'ansicolors', 'httplib2', 'mozfile',
+                        'mozprofile', 'mozrunner', 'BeautifulSoup', 'paramiko',
+                        'configurator', 'prettylogger']
 )
